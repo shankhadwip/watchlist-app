615a7bada83b730cd3d265817cc45aa4fd644c61
shankha2001 <shankhadwip1898@gmail.com>
2025-05-14 12:45:12 +0530

    Base changes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/watchlist-app/",
+  base: "/",
   plugins: [tailwindcss(),react()],
 })
